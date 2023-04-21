b7e7ffb4bc9778e2c0336d97ff0a91cd17509495
emandaseifu <115669613+emandaseifu@users.noreply.github.com>
2023-04-21 03:39:03 -0400

    Update Input.java

--- Input.java
+++ Input.java
@@ -1,4 +1,4 @@
-package prj5;
+package mediavis;
 
 import java.io.FileNotFoundException;
 import java.text.ParseException;
@@ -7,8 +7,16 @@ import java.util.LinkedList;
 import java.util.Scanner;
 import java.io.FileReader;
 
+/**
+ * The Input class 
+ *
+ * @author Emanda Seifu
+ * @author Shreyas Kunaparaju
+ * @author Arnav Sharma
+ * @version 04.28.2023
+ */
 public class Input {
-    private String filename
+    private String filename;
     public static void main(String[] args) throws Exception {
 
         if (args.length == 1) {
