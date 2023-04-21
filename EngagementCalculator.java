7cd0740b9dcd33e238d824c8788018672a5b8a7e
emandaseifu <115669613+emandaseifu@users.noreply.github.com>
2023-04-20 21:08:52 -0400

    Create EngagementCalculator.java

---
+++ EngagementCalculator.java
@@ -0,0 +1,58 @@
+package prj5;
+
+/**
+ * The Engagement Calculator class 
+ *
+ * @author Emanda Seifu
+ * @author 
+ * @author 
+ * @version 04.28.2023
+ */
+
+import java.text.DecimalFormat;
+import list.AList;
+import prj5.EngagementReader;
+import prj5.Influencer;
+import prj5.Statistics;
+
+public class EngagementCalculator {
+    public static final int RATING;
+    
+    double rate = 0;
+    double reach = 0;
+    double sum = 0;
+    int totalEng = 0;
+    int totalFol = 0;
+    Statistics stats;
+    
+    public static final int RATE = 3;        
+    
+    public EngagementCalculator(Statistics comments) throws IllegalException {
+        this.RATING = RATING;
+        
+    }
+    
+    public double getEngagementRate() {
+        rate = (totalEng/ totalFol) * (0.1);
+        return rate;
+    }
+    
+    public double getEngagementReach() {
+        reach = (totalEng/ reach) * (0.1);
+        return reach;
+    }
+    
+    public double getEngagement(Statistics other) {
+        sum = other.getComments() + other.getLikes();
+        return sum;
+    }
+    
+    public int getFollowers(Statistics other) {
+        return other.getFollowers() ;
+    }
+    
+    public int getReach(Statistics other) {
+        return other.getViews();
+    }
+ 
+}
